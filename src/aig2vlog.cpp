d03c641b63b3058808307b971f465eeb44a0b47b
George Rennie <georgerennie@gmail.com>
2024-09-28 12:11:49 +0100

    support proper names etc in aig2vlog

--- src/aig2vlog.cpp
+++ src/aig2vlog.cpp
@@ -3,6 +3,7 @@
 #include "aiger++.hpp"
 #include "clipp/clipp.h"
 #include "fmt/core.h"
+#include <cassert>
 
 struct Options {
 	std::string input_path;
@@ -35,47 +36,81 @@ int main(int argc, char* argv[]) {
 	if (!aig)
 		return EXIT_FAILURE;
 
-	const auto net = [](const Aiger::Lit lit) -> std::string {
+	const auto lhs = [](const Aiger::Lit lit) -> std::string {
+		assert(!aiger_is_constant(lit));
+		assert(!aiger_sign(lit));
+		return fmt::format("_n{}", aiger_lit2var(lit));
+	};
+
+	const auto rhs = [&lhs](const Aiger::Lit lit) -> std::string {
 		if (aiger_is_constant(lit))
 			return lit == aiger_false ? "1'b0" : "1'b1";
-		return fmt::format("{}n{}", aiger_sign(lit) ? "~" : "", aiger_lit2var(lit));
+		return fmt::format("{}{}", aiger_sign(lit) ? "~" : "", lhs(aiger_strip(lit)));
 	};
 
 	fmt::println(R"(module top()");
 
-	for (const auto input : Aiger::inputs(aig))
-		fmt::println("\tinput wire {},", net(input.lit));
-	fmt::println("\tinput wire clk");
+	const auto net_name = [](const size_t idx, const char* name, const char prefix) -> std::string {
+		if (name == nullptr)
+			return fmt::format("_{}{}", prefix, idx);
+		return fmt::format(R"(\{} )", name);
+	};
+
+	for (size_t i = 0; i < aig->num_inputs; i++)
+		fmt::println("\tinput wire {},", net_name(i, aig->inputs[i].name, 'i'));
+	for (size_t i = 0; i < aig->num_outputs; i++)
+		fmt::println("\toutput wire {},", net_name(i, aig->outputs[i].name, 'o'));
+	fmt::println("\tinput wire clk,");
+	fmt::println("\tinput wire rst");
 	fmt::println(");");
 
-	// Declare all nets
-	for (const auto gate : Aiger::gates(aig))
-		fmt::println("\twire {};", net(gate.lhs));
-	for (const auto latch : Aiger::latches(aig))
-		fmt::println("\treg {};", net(latch.lit));
+	// Connect inputs to internal wires
+	for (size_t i = 0; i < aig->num_inputs; i++)
+		fmt::println("wire {} = {};", lhs(aig->inputs[i].lit),net_name(i, aig->inputs[i].name, 'i'));
+
+	// Declare all registers
+	for (size_t i = 0; i < aig->num_latches; i++) {
+		const auto& latch = aig->latches[i];
+		const auto name = net_name(i, latch.name, 'l');
+		fmt::println("reg {}; wire {} = {};", name, lhs(latch.lit), name);
+	}
 
 	// Comb gates
-	for (const auto gate : Aiger::gates(aig))
-		fmt::println("assign {} = {} & {};", net(gate.lhs), net(gate.rhs0), net(gate.rhs1));
+	for (const auto& gate : Aiger::gates(aig))
+		fmt::println("wire {} = {} & {};", lhs(gate.lhs), rhs(gate.rhs0), rhs(gate.rhs1));
 
 	fmt::println("always @(posedge clk) begin");
-	for (const auto latch : Aiger::latches(aig))
-		fmt::println("\t{} <= {};", net(latch.lit), net(latch.next));
+	for (size_t i = 0; i < aig->num_latches; i++)
+		fmt::println("\t{} <= {};", net_name(i, aig->latches[i].name, 'l'), rhs(aig->latches[i].next));
+	fmt::println("\tif (rst) begin");
+	for (size_t i = 0; i < aig->num_latches; i++) {
+		const auto& latch = aig->latches[i];
+		if (latch.reset != latch.lit)
+			fmt::println("\t\t{} <= {};", net_name(i, aig->latches[i].name, 'l'), rhs(latch.reset));
+	}
+	fmt::println("\tend");
 	fmt::println("end");
 
-	fmt::println("initial begin");
-	for (const auto latch : Aiger::latches(aig))
-		if (latch.reset != latch.lit)
-			fmt::println("\t{} = {};", net(latch.lit), net(latch.reset));
-	fmt::println("end");
+	const auto prop_name = [](const char* name) -> std::string {
+		if (name == nullptr)
+			return "";
+		else
+			return fmt::format("{}: ", name);
+	};
+
+	// Connect internal wires to outputs
+	for (size_t i = 0; i < aig->num_outputs; i++)
+		fmt::println("assign {} = {};", net_name(i, aig->outputs[i].name, 'i'), rhs(aig->outputs[i].lit));
 
 	fmt::println("always @* begin");
-	for (const auto constraint : Aiger::constraints(aig))
-		fmt::println("\tassume({});", net(constraint.lit));
-	for (const auto bad: Aiger::bads(aig))
-		fmt::println("\tassert({});", net(aiger_not(bad.lit)));
+	fmt::println("\tif (!rst) begin");
+	for (const auto& constraint : Aiger::constraints(aig))
+		fmt::println("\t\t{}assume({});", prop_name(constraint.name), rhs(constraint.lit));
+	for (const auto& bad: Aiger::bads(aig))
+		fmt::println("\t\t{}assert({});", prop_name(bad.name), rhs(aiger_not(bad.lit)));
 	assert(aig->num_justice == 0);
 	assert(aig->num_fairness == 0);
+	fmt::println("\tend");
 	fmt::println("end");
 
 	fmt::println("endmodule");
